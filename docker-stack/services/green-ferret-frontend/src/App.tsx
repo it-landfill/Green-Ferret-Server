6bb7b1e4994fbfbd798d9249abeadffff0605182
lorenzocrespan <lorenzo.crespan@gmail.com>
2023-06-19 15:33:28 +0200

    Add data chart

--- docker-stack/services/green-ferret-frontend/src/App.tsx
+++ docker-stack/services/green-ferret-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import "./tailwind-output.css";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import {HeatmapLayerFactory} from "@vgrid/react-leaflet-heatmap-layer";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const HeatmapLayer = HeatmapLayerFactory<[number, number, number]>()
 
@@ -39,40 +40,112 @@ function App() {
   // Use effect for the data points index (if the index changes, the data points will be updated)
   React.useEffect(() => {
     console.log("dataPoints: " + dataPoints[dataPointsIndex]);
-    generateMap([dataPoints[dataPointsIndex]]);
   }, [dataPointsIndex]);
 
-  // Generate the map
-  function generateMap(dataPoint: [number, number, number][]) {
-    
-    return (
-      <MapContainer style={{
-        height: "100%"
-      }} center={[44.4949, 11.3426]} zoom={13} scrollWheelZoom={false}>
-        <HeatmapLayer
-            fitBoundsOnLoad
-            fitBoundsOnUpdate
-            points = {dataPoint}
-            longitudeExtractor={m => m[1]}
-            latitudeExtractor={m => m[0]}
-            intensityExtractor={m => m[2]}
-            radius={30}
-            gradient={{ 0.2: 'blue', 0.7: 'yellow', 1.0: 'red' }}
-            opacity={0.75}
-        />
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      </MapContainer>
-    );
-  }
+  const data = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
 
+  function renderLineChart() {
+    return(
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis dataKey="pv" yAxisId="pv" stroke="#8884d8" orientation="left"/>
+          <YAxis dataKey="uv" yAxisId="uv" stroke="#82ca9d" orientation="left"/>
+          
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" yAxisId="pv" stroke="#8884d8" activeDot={{ r: 6 }}/>
+          <Line type="monotone" dataKey="uv" yAxisId="uv" stroke="#82ca9d" activeDot={{ r: 6 }}/>
+        </LineChart>
+      </ResponsiveContainer>
+    );
+  };
+
+  function renderLineChartSync(id: [string, string]) {
+    return(
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          data={data}
+          syncId={id[1]}
+          margin={{
+            top: 5,
+            right: 20,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis dataKey= {id[0]} yAxisId={id[0]} orientation="left"/>
+    
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey={id[0]} yAxisId={id[0]} stroke="#8884d8" />
+        </LineChart>
+      </ResponsiveContainer>
+    );
+  };
 
 
   return (
     <div className="App">
       <div className="flex flex-col h-screen p-2 gap-2">
         <h1 className="text-4xl font-bold text-left text-blue-500">Green Ferrett - Heatmap</h1>
-        <div className="flex h-3/4 items-center justify-center gap-2">
-          <div className="w-2/3 h-full bg-red-200 rounded-lg overflow-hidden">
+        
+        <div className="flex h-1/2 items-center justify-center gap-2">
+          <div className="w-4/5 h-full bg-red-200 rounded-lg overflow-hidden">
             <MapContainer style={{
               height: "100%"
             }} center={[44.4949, 11.3426]} zoom={13} scrollWheelZoom={false}>
@@ -90,7 +163,7 @@ function App() {
               <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             </MapContainer>
           </div>
-          <div className="w-1/3 h-full rounded-lg">
+          <div className="w-1/5 h-full rounded-lg">
             <div className="flex flex-col h-full gap-2">
               <div className="flex flex-col gap-2 m-4">
                 <h2 className="text-2xl font-bold text-left text-blue-500">Filtro heatmap</h2>
@@ -128,7 +201,58 @@ function App() {
             </div>
           </div>
         </div>
-       
+
+        <div className="flex h-1/2 items-center justify-center gap-2 m-4">
+          <div className="w-4/5 h-full rounded-lg">
+            {renderLineChart()}
+          </div>
+          <div className="w-1/5 h-full rounded-lg">
+            <div className="flex flex-col h-full gap-2">
+              <div className="flex flex-col gap-2 m-4">
+                <h2 className="text-2xl font-bold text-left text-blue-500">Filtro heatmap</h2>
+                <div className="flex items-center">
+                    <input checked id="default-radio-1" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" />
+                    <label htmlFor="default-radio-1" className="ml-3 text-md font-medium text-gray-900 dark:text-gray-600">Visualizza la heatmap 1</label>
+                </div>
+                <div className="flex items-center">
+                  <input checked id="default-radio-2" type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" />
+                    <label htmlFor="default-radio-2" className="ml-3 text-md font-medium text-gray-900 dark:text-gray-600">Visualizza la heatmap 2</label>
+                </div>
+              </div> 
+            </div>
+          </div>
+        </div>
+
+      </div>
+      
+      <div className="flex flex-col h-screen p-2 gap-2">
+        <h1 className="text-4xl font-bold text-left text-blue-500">Green Ferrett - Heatmap</h1>
+        <div className="grid grid-cols-2 h-screen p-2 gap-2">
+          <div className="items-center justify-center gap-2 m-4">
+            <div className="h-3/4 rounded-lg">
+              {renderLineChartSync(["uv", "sync"])}
+              <p className="text-md font-bold text-left text-blue-500 m-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, eget aliquam nisl nisl sit amet nunc. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, eget aliquam nisl nisl sit amet nunc.</p>
+            </div>
+          </div>
+          <div className="items-center justify-center gap-2 m-4">
+            <div className="h-3/4 rounded-lg">
+              {renderLineChartSync(["pv", "sync"])}
+              <p className="text-md font-bold text-left text-blue-500 m-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, eget aliquam nisl nisl sit amet nunc. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, eget aliquam nisl nisl sit amet nunc.</p>
+            </div>
+          </div>
+          <div className="items-center justify-center gap-2 m-4">
+            <div className="h-3/4 rounded-lg">
+              {renderLineChartSync(["amt", "sync"])}
+              <p className="text-md font-bold text-left text-blue-500 m-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, eget aliquam nisl nisl sit amet nunc. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, eget aliquam nisl nisl sit amet nunc.</p>
+            </div>
+          </div>
+          <div className="items-center justify-center gap-2 m-4">
+            <div className="h-3/4 rounded-lg">
+              {renderLineChartSync(["pv", "sync"])}
+              <p className="text-md font-bold text-left text-blue-500 m-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, eget aliquam nisl nisl sit amet nunc. Nullam euismod, nisl eget aliquam ultricies, nunc nisl aliquet nunc, eget aliquam nisl nisl sit amet nunc.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
