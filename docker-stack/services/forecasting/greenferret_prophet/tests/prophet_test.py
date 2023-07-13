df4c50bbbb30e467387147f235c02595ab6b8c3b
lorenzocrespan <lorenzo.crespan@gmail.com>
2023-07-13 19:53:16 +0200

    Enhanced prophet implementation

--- docker-stack/services/forecasting/greenferret_prophet/tests/prophet_test.py
+++ docker-stack/services/forecasting/greenferret_prophet/tests/prophet_test.py
@@ -5,50 +5,56 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 import time
 from datetime import datetime
 
-token = "zQ0CgptaJgXzlNlRqCgtv89hcGEtDSf6B3BHM_E8vYhqnhOZJgYCCOSA8inN4tyh__4TnEHG3ERyHK7fTMl4jw=="
-bucket = "TestBucket"
-org = "TestOrganization"
+token = "mGXC9hh6IQXuFn6xvF41OZWesNIvfMFhVkJEm2fRXMzaDE7V56qUUS2obpmFLgI2QAkQOxHz02h0_D-y4VxqIQ=="
+bucket = "GreenFerret"
+org = "ITLandfill"
 client = InfluxDBClient(url="http://localhost:8086", token=token, org=org)
 
-# Function to set up the InfluxDB client
+# Function to set up the InfluxDB client and return the write and query APIs.
 def setup_influxdb_client():
     query_api = client.query_api()
     write_api = client.write_api(write_options=SYNCHRONOUS)
     return write_api, query_api
 
-# Function to query the InfluxDB database
+# Function to query the InfluxDB database and return a DataFrame.
 def query_influxdb(query_api):
-    query = 'from(bucket: "TestBucket")' \
-        '|> range(start:2023-06-30T00:00:00Z, stop:2023-06-30T19:00:00Z)'\
-        '|> filter(fn: (r) => r["_measurement"] == "airSensors")' \
-        '|> filter(fn: (r) => r["_field"] == "humidity")' \
-        '|> filter(fn: (r) => r["sensor_id"] == "TLM0100")'
+    # Query the InfluxDB database.
+    # Example query:
+    #   From the bucket named "GreenFerret", select the "airSensors" measurement about "humidity" and 
+    #   mean all the value from different sensors in the same period with a 5 minutes interval.
+    query = 'from(bucket: "GreenFerret")' \
+            '|> range(start:2023-07-13T00:00:00Z, stop:2023-07-13T19:00:00Z)' \
+            '|> filter(fn: (r) => r["_measurement"] == "airSensors")' \
+            '|> filter(fn: (r) => r["_field"] == "humidity")' \
+            '|> drop(columns: ["sensor_id"])' \
+            '|> aggregateWindow(every: 5m, fn: mean, createEmpty: false)' \
+            '|> yield(name: "mean")'
+    # Execute the query and return the result.
     result = client.query_api().query(org=org, query=query)
     raw = []
+    # Iterate over the result tables and records.
     for table in result:
         for record in table.records:
+            # Append the value and time of the record to the raw list.
             raw.append((record.get_value(), record.get_time()))
+    # Create a DataFrame from the result.
     df=pd.DataFrame(raw, columns=['y','ds'], index=None)
+    # Convert the time column to datetime64[ns] and the value column to float.
     df['ds'] = df['ds'].values.astype('datetime64[ns]')
     df['y'] = df['y'].values.astype('float')
     df.set_index('ds')
-
     return df
 
 # Fit the model by instantiating a new Prophet object and passing in the historical DataFrame.
 def fit_prophet_model(df):
-    # Print the first 5 rows of the DataFrame.
-    print(df.head())
     m = Prophet(interval_width=0.95)
     # If there's some type of error, see: https://github.com/facebook/prophet/issues/1568#issuecomment-660328884
     m.fit(df)
     future = m.make_future_dataframe(periods=24, freq='H')
     forecast = m.predict(future)
-    forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-    print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
     return forecast
 
-# Function to convert the forecast Dataframe to Line Protocol.
+# Function to convert the forecast Dataframe to Line Protocol, which is the format used by InfluxDB.
 def convert_forecast_to_list(forecast):
     forecast['measurement'] = "views"
     cp = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper','measurement']].copy()
@@ -60,7 +66,6 @@ def convert_forecast_to_list(forecast):
          + "yhat_upper=" + str(cp["yhat_upper"][d])
          + " " + str(int(time.mktime(cp['ds'][d].timetuple()))) + "000000000" for d in range(len(cp))
     ]
-    print(lines)
     return lines
 
 # Function to write the forecast to InfluxDB.
