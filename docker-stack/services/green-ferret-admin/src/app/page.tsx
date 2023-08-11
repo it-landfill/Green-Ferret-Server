01e740c69fc52bad4e5a78de35cf70cd0e5e1b8a
Alessandro Benetton <aleben98@gmail.com>
2023-08-11 16:18:56 +0200

    [feature/GF-35] GF Admin init Next

---
+++ docker-stack/services/green-ferret-admin/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+	return (
+		<main className="bg-blue-200 flex min-h-screen max-h-screen flex-col items-center justify-between">
+			<p className="text-3xl pt-3">Green Ferret - Admin Panel</p>
+			<div className="w-full grow p-5">
+				<div className="bg-white rounded">
+					Lista dei dispositivi
+				</div>
+			</div>
+		</main>
+	);
+}
