3938f5b8b11979cb2904f1bebe0627ebec427d1b
lorenzocrespan <lorenzo.crespan@gmail.com>
2023-08-29 22:39:25 +0200

    In progress dynamic line chart

--- docker-stack/services/green-ferret-frontend/src/components/lineChartsControls/LCLineChartsComponents/LCLineMonoGraph.tsx
+++ docker-stack/services/green-ferret-frontend/src/components/lineChartsControls/LCLineChartsComponents/LCLineMonoGraph.tsx
@@ -1,9 +1,8 @@
 import React from "react";
+
 import {
   LineChart,
-  Line,
   XAxis,
-  YAxis,
   CartesianGrid,
   Tooltip,
   Legend,
@@ -12,137 +11,68 @@ import {
 
 import { InfluxAccess } from "../../../utils/InfluxAccess";
 
+import LineYAxis from "./LCLineMonoGraphComponents/LCLineYAxis";
+
 interface LineMonoGraphProps {
   dataPointsLineChart: InfluxAccess.Measurement[];
   lineChartState: any;
 }
 
 function LineMonoGraph(props: LineMonoGraphProps) {
+  const [LineList, setLineList] = React.useState<any[]>([]);
+
+  // Simplified date format (DD/MM HH:MM).
+  const dateFormat = (date: Date) => {
+    return `${date.getDate()}/${date.getMonth() + 1} ${date
+      .getHours()
+      .toString()
+      .padStart(2, "0")}:${date.getMinutes().toString().padStart(2, "0")}`;
+  };
+
+  React.useEffect(() => {
+    // For each props.lineChartState key, create a Line component.
+    const LineList = Object.keys(props.lineChartState).map((key) => (
+      <LineYAxis
+        key={key}
+        lineChartState={props.lineChartState}
+        lineChartTopic={key}
+        lineColor="#8884d8"
+        lineOrientation="left"
+      />
+    ));
+    setLineList(LineList);
+  }, [props.lineChartState]);
+
   return (
     <ResponsiveContainer width="100%" height="100%">
       <LineChart
         data={props.dataPointsLineChart}
         margin={{
-          top: 5,
+          top: 25,
           right: 30,
           left: 20,
-          bottom: 5,
+          bottom: 25,
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="time" tick={{ fontSize: 12 }} dy={10} />
 
-        {props.lineChartState.temperature && (
-          <YAxis
-            dataKey="temperature"
-            yAxisId="temperature"
-            stroke="#8884d8"
-            orientation="left"
-            tick={{ fontSize: 12 }}
-          />
-        )}
-        {props.lineChartState.pressure && (
-          <YAxis
-            dataKey="pressure"
-            yAxisId="pressure"
-            stroke="#82ca9d"
-            orientation="left"
-            tick={{ fontSize: 12 }}
-          />
-        )}
-        {props.lineChartState.humidity && (
-          <YAxis
-            dataKey="humidity"
-            yAxisId="humidity"
-            stroke="#CA829D"
-            orientation="left"
-            tick={{ fontSize: 12 }}
-          />
-        )}
-        {props.lineChartState.eco && (
-          <YAxis
-            dataKey="eco2"
-            yAxisId="eco"
-            stroke="#9D82CA"
-            orientation="right"
-            tick={{ fontSize: 12 }}
-          />
-        )}
-        {props.lineChartState.tvoc && (
-          <YAxis
-            dataKey="tvoc"
-            yAxisId="tvoc"
-            stroke="#CA9D82"
-            orientation="right"
-            tick={{ fontSize: 12 }}
-          />
-        )}
-        {props.lineChartState.aqi && (
-          <YAxis
-            dataKey="aqi"
-            yAxisId="aqi"
-            stroke="#6342FC"
-            orientation="right"
-            tick={{ fontSize: 12 }}
-          />
-        )}
+        <XAxis
+          dataKey="time"
+          tickFormatter={dateFormat}
+          tick={{ fontSize: 12 }}
+          dy={10}
+        />
+
+        <LineYAxis
+          key="humidity"
+          lineChartState={props.lineChartState}
+          lineChartTopic="humidity"
+          lineColor="#8884d8"
+          lineOrientation="left"
+        />
 
         <Tooltip />
         <Legend />
-        {props.lineChartState.temperature && (
-          <Line
-            type="monotone"
-            dataKey="temperature"
-            yAxisId="temperature"
-            stroke="#8884d8"
-            activeDot={{ r: 6 }}
-          />
-        )}
-        {props.lineChartState.pressure && (
-          <Line
-            type="monotone"
-            dataKey="pressure"
-            yAxisId="pressure"
-            stroke="#82ca9d"
-            activeDot={{ r: 6 }}
-          />
-        )}
-        {props.lineChartState.humidity && (
-          <Line
-            type="monotone"
-            dataKey="humidity"
-            yAxisId="humidity"
-            stroke="#CA829D"
-            activeDot={{ r: 6 }}
-          />
-        )}
-        {props.lineChartState.eco && (
-          <Line
-            type="monotone"
-            dataKey="eco2"
-            yAxisId="eco"
-            stroke="#9D82CA"
-            activeDot={{ r: 6 }}
-          />
-        )}
-        {props.lineChartState.tvoc && (
-          <Line
-            type="monotone"
-            dataKey="tvoc"
-            yAxisId="tvoc"
-            stroke="#CA9D82"
-            activeDot={{ r: 6 }}
-          />
-        )}
-        {props.lineChartState.aqi && (
-          <Line
-            type="monotone"
-            dataKey="aqi"
-            yAxisId="aqi"
-            stroke="#6342FC"
-            activeDot={{ r: 6 }}
-          />
-        )}
       </LineChart>
     </ResponsiveContainer>
   );
